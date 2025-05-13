5e96389bb7b97467a79f4f8ced464502917abb60
orjon <github@orjon.com>
2025-05-14 00:24:07 +0100

    MongoDB setup. Register user route working

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import connectDB from './config/db.js'
+import userRoutes from './routes/userRoutes.js'
+
+dotenv.config()
+
+connectDB()
+
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/api/users', userRoutes)
+
+app.get('/', (req, res) => {
+  res.send('Server is running')
+})
+
+app.use(notFound)
+app.use(errorHandler)
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
